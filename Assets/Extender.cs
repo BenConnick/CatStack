124fa4f65561889f0e5bc005b6869cf3d7b48cb1
Ben C <ben@expmaker.com>
2016-10-04 23:52:58 -0400

    extender arm for FPSController

---
+++ Assets/Extender.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Extender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(0,0, transform.localPosition.z + (Input.mouseScrollDelta.y) / 20);
+	}
+}
